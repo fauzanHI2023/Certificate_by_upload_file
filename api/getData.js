7aaa5b15cfab6a1ea3b9bf659d843f1f90001ad9
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-11-15 15:06:42 +0700

    update Sertifikanbiufefjklwd;le

--- api/getData.js
+++ api/getData.js
@@ -7,7 +7,7 @@ const { MONGODB_URI, MONGODB_DB } = process.env;
 export default async function handler(req, res) {
   try {
     // Connect to the MongoDB cluster
-    const client = await MongoClient.connect(process.env.MONGODB_URI, {
+    const client = await MongoClient.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
