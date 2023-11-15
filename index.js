198094f8399f45f26b2cee39c590df122c1bbbaf
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-11-15 17:00:18 +0700

    update3

--- index.js
+++ index.js
@@ -46,7 +46,6 @@ const generatePDF = async (name, certificateNumber) => {
      color: rgb(0.2, 0.84, 0.67),
    });
 
-   const uniqueNumber = generateUniqueNumber();
    const certificateNumber = certificateCounter++;
    firstPage.drawText(`No. ${formattedDate} 00${certificateNumber} ${uniqueNumber}`, {
     x: 300,
