747577c0ad6e074053e8c24032699e0e0b91921b
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-11-15 15:05:10 +0700

    update Sertifikanbiufefjkrnf

---
+++ api/getData.js
@@ -0,0 +1,35 @@
+// api/getdata.js
+
+import { MongoClient } from 'mongodb';
+
+const { MONGODB_URI, MONGODB_DB } = process.env;
+
+export default async function handler(req, res) {
+  try {
+    // Connect to the MongoDB cluster
+    const client = await MongoClient.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    // Access the specified database
+    const db = client.db(MONGODB_DB);
+
+    // Access the collection (replace 'yourCollection' with the actual collection name)
+    const collection = db.collection('collection-tanampohon');
+
+    // Perform a query to retrieve data (replace {} with your query)
+    const result = await collection.find({}).toArray();
+
+    // Send the data as a JSON response
+    res.status(200).json({ data: result });
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    // Close the MongoDB connection
+    if (client) {
+      await client.close();
+    }
+  }
+}
