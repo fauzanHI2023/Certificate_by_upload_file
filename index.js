5fd3cabaa02c9a5b75abbece2ff2b055b28cb2ca
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-11-15 18:45:37 +0700

    update14

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const generatePDF = async (name, certificateNumber) => {
    const currentDate = new Date();
    const formattedDate = `${currentDate.getFullYear()}${currentDate.getMonth() + 1}${currentDate.getDate()}`;
    firstPage.drawText(name, {
-     x: 280,
+     x: 300,
      y: 370,
      size: 20,
      font: SanChezFont ,
@@ -49,7 +49,7 @@ const generatePDF = async (name, certificateNumber) => {
 
    const uniqueNumber = generateUniqueNumber();
    firstPage.drawText(`No. ${formattedDate} 00${certificateNumber} ${uniqueNumber}`, {
-    x: 400,
+    x: 500,
     y: 270,
     size: 20,
     font: SanChezFont ,
