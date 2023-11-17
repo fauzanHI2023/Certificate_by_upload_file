6708463907f9929d69a44e1e3fb3ca05dd8ac243
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-11-17 11:11:50 +0700

    update87

--- api/getNextCertificateNumber.js
+++ api/getNextCertificateNumber.js
@@ -2,22 +2,28 @@ import { MongoClient } from 'mongodb';
 
 export default async function handler(req, res) {
   try {
+    // Connect to the MongoDB cluster
     const client = await MongoClient.connect(process.env.MONGODB_URI);
-
+  
+    // Access the specified database
     const db = client.db('certificate-hi-tanampohon');
+  
+    // Access the collection (replace 'yourCollection' with the actual collection name)
     const collection = db.collection('tanampohon');
-
-    // Find the count and increment it
-    const result = await collection.findOneAndUpdate(
-      { type: 'certificateCount' },
-      { $inc: { count: 1 } },
-      { returnDocument: 'after' }
-    );
-
-    const certificateNumber = result.value.count;
-    res.status(200).json({ certificateNumber });
+  
+    // Perform a query to retrieve the next certificate number
+    const result = await collection.countDocuments();
+    const nextCertificateNumber = result + 1;
+  
+    // Send the next certificate number as a JSON response
+    res.status(200).json({ nextCertificateNumber });
   } catch (error) {
     console.error('Error getting next certificate number:', error);
     res.status(500).json({ error: 'Internal Server Error' });
-  } 
+  } finally {
+    // Close the MongoDB connection
+    if (client) {
+      await client.close();
+    }
+  }
 }
