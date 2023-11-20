93c89f21851e5980988633f5d7716c371b809a5c
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-11-20 16:28:50 +0700

    update1117

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,10 +1,1 @@
-import { Resend } from 'resend';
-
-const resend = new Resend('re_DgNVekbB_6atKyyq2GRJC997b6z6GqUg6');
-
-resend.emails.send({
-  from: 'admin@human-initiative.org',
-  to: 'fabismillah0@gmail.com',
-  subject: 'Hello World',
-  html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
-});
+alert("Fauzan Aldi");
