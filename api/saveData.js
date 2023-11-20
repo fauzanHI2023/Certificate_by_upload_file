dac706b7817e83dd265540cb8ac0d77facd877b3
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-11-20 11:21:25 +0700

    update1106

--- api/saveData.js
+++ api/saveData.js
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb';
+import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -30,6 +31,30 @@ export default async function handler(req, res) {
       });
 
       client.close();
+
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'antoalfian004@gmail.com', // Replace with your Gmail email
+          pass: '345678Aldi', // Replace with your Gmail password
+        },
+      });
+
+      const mailOptions = {
+        from: 'antoalfian004@gmail.com', // Replace with your Gmail email
+        to: email,
+        subject: 'Certificate Information',
+        text: `Dear ${name}, your certificate with number ${certificateNumber} is attached.`,
+        attachments: [
+          {
+            filename: 'Certificate-TanamPohon.pdf',
+            content: pdfDataUri.replace(/^data:application\/pdf;base64,/, ''),
+            encoding: 'base64',
+          },
+        ],
+      };
+
+      await transporter.sendMail(mailOptions);
 
       return res.status(200).json({ success: true, data: result.ops[0] });
     } catch (error) {
