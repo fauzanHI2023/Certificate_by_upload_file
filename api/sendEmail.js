442edf52d2e4cedf2fa2e8194e85ddeb56693e39
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-11-20 17:38:27 +0700

    update1122

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,44 +1,42 @@
-// Import libraries
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { name, email, certificateNumber, pdfDataUri } = req.body || {};
-    
-        if (!name || !email || !certificateNumber || !pdfDataUri) {
-          console.error('Invalid payload:', req.body);
-          return res.status(400).json({ error: 'Invalid payload' });
-        }
-    
-        console.log('Sending email to:', email);
-
-    // Konfigurasi transporter untuk layanan email
-    const transporter = nodemailer.createTransport({
-      host: 'smtp-mail.outlook.com', // Ganti dengan host yang sesuai
-      secure: false,
-      port: 587,                                      
-      auth: {
-        user: 'Admin@human-initiative.org', // Ganti dengan email pengirim
-        pass: '1234Pkpu', // Ganti dengan password email pengirim
-      },
-    });
-
-    // Opsi email
-    const mailOptions = {
-      from: 'admin@human-initiative.org', // Ganti dengan email pengirim
-      to: email,
-      subject: 'Certificate Information',
-      text: `Dear ${name}, your certificate with number ${certificateNumber} is attached.`,
-      attachments: [
-        {
-          filename: 'Certificate-TanamPohon.pdf',
-          content: pdfDataUri.replace(/^data:application\/pdf;base64,/, ''),
-          encoding: 'base64',
-        },
-      ],
-    };
-
+  if (req.method === 'POST') {
     try {
+      const { name, email, certificateNumber, pdfDataUri } = req.body || {};
+
+      if (!name || !email || !certificateNumber || !pdfDataUri) {
+        return res.status(400).json({ error: 'Invalid payload: Missing required fields' });
+      }
+
+      console.log('Sending email to:', email);
+
+      // Konfigurasi transporter untuk layanan email
+      const transporter = nodemailer.createTransport({
+        host: 'smtp-mail.outlook.com', // Ganti dengan host yang sesuai
+        secure: false,
+        port: 587,
+        auth: {
+          user: 'Admin@human-initiative.org', // Ganti dengan email pengirim
+          pass: '1234Pkpu', // Ganti dengan password email pengirim
+        },
+      });
+
+      // Opsi email
+      const mailOptions = {
+        from: 'admin@human-initiative.org', // Ganti dengan email pengirim
+        to: email,
+        subject: 'Certificate Information',
+        text: `Dear ${name}, your certificate with number ${certificateNumber} is attached.`,
+        attachments: [
+          {
+            filename: 'Certificate-TanamPohon.pdf',
+            content: pdfDataUri.replace(/^data:application\/pdf;base64,/, ''),
+            encoding: 'base64',
+          },
+        ],
+      };
+
       // Mengirim email
       const info = await transporter.sendMail(mailOptions);
 
@@ -52,4 +50,4 @@ export default async function handler(req, res) {
   } else {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
-};
+}
