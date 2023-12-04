e939917456428d288b70fe591284e87e65463355
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-12-04 14:21:03 +0700

    Update insert

--- api/saveData.js
+++ api/saveData.js
@@ -29,7 +29,6 @@ export default async function handler(req, res) {
         email: email,
         telepon: telepon,
         certificateNumber: certificateNumber,
-        pdfDataUri: pdfDataUri,
       });
 
       client.close();
