cc91c1b40afeadc8ff46ccbdefae724e2ca74464
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2024-01-04 11:16:10 +0700

    coba test6

--- api/getNextCertificateNumber.js
+++ api/getNextCertificateNumber.js
@@ -1,24 +1,27 @@
 import { MongoClient } from 'mongodb';
 
 export default async function handler(req, res) {
+  // Set CORS headers to allow requests from your client domain
+  res.setHeader('Access-Control-Allow-Origin', 'https://certificate-by-upload-file.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
   try {
     // Connect to the MongoDB cluster
-    const client = await MongoClient.connect(process.env.MONGODB_URI);
-  
+    const client = await MongoClient.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     // Access the specified database
     const db = client.db('certificate-hi-tanampohon');
-  
+
     // Access the collection (replace 'yourCollection' with the actual collection name)
     const collection = db.collection('tanampohon-collection');
-  
+
     // Perform a query to retrieve the next certificate number
     const result = await collection.countDocuments();
     const nextCertificateNumber = result + 1;
 
-    // Set CORS headers to allow requests from your client domain
-    res.setHeader('Access-Control-Allow-Origin', 'https://certificate-by-upload-file.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-  
     // Send the next certificate number as a JSON response
     res.status(200).json({ nextCertificateNumber });
   } catch (error) {
--- index.js
+++ index.js
@@ -101,7 +101,7 @@ const getNextCertificateNumber = async () => {
   // For example, you can use a fetch request to your server
   try {
     const response = await fetch(
-      "https://certificate-by-upload-file.vercel.app/api/getNextCertificateNumberExcel"
+      "https://certificate-by-upload-file.vercel.app/api/getNextCertificateNumber"
     );
     if (!response.ok) {
       throw new Error(
