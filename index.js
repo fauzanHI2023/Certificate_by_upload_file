27a6d8b0a7cafa49cca9deeee734657f0edfac91
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2024-01-04 16:18:21 +0700

    coba test12

--- index.js
+++ index.js
@@ -14,42 +14,41 @@ downloadCertificatesBtn.onclick = function () {
   downloadCertificates();
 };
 
-const { PDFDocument, rgb, degrees } = PDFLib;
-
 submitBtn.addEventListener("click", async () => {
   const file = fileInput.files[0];
 
-  if (file) {
-    try {
-      const workbook = await readExcelFile(file);
-      const sheet = workbook.Sheets[workbook.SheetNames[0]];
-      data = XLSX.utils.sheet_to_json(sheet);
-
-      for (let i = 0; i < data.length; i++) {
-        const rowData = data[i];
-
-        if (isValidRow(rowData)) {
-          const { name, email, telepon } = rowData;
-          const nextCertificateNumber = await getNextCertificateNumber();
-          const pdfDataUri = await generatePDF(name, nextCertificateNumber);
-
-          hideFormDisplay();
-          await sendCertificateData(name, email, telepon, nextCertificateNumber, pdfDataUri);
-
-          showFormDisplay();
-          fileInput.value = ""; // Clear file input
-        } else {
-          console.error("Missing required data in row ", i + 1);
-        }
-      }
-
-      showDownloadButton();
-      showPopup();
-    } catch (error) {
-      console.log("Error Occurred", error);
-    }
-  } else {
+  if (!file) {
     console.error("Please select a file");
+    return;
+  }
+
+  try {
+    const workbook = await readExcelFile(file);
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    data = XLSX.utils.sheet_to_json(sheet);
+
+    for (let i = 0; i < data.length; i++) {
+      const rowData = data[i];
+
+      if (isValidRow(rowData)) {
+        const { name, email, telepon } = rowData;
+        const nextCertificateNumber = await getNextCertificateNumber();
+        const pdfDataUri = await generatePDF(name, nextCertificateNumber);
+
+        hideFormDisplay();
+        await sendCertificateData(name, email, telepon, nextCertificateNumber, pdfDataUri);
+
+        showFormDisplay();
+        fileInput.value = ""; // Clear file input
+      } else {
+        console.error("Missing required data in row ", i + 1);
+      }
+    }
+
+    showDownloadButton();
+    showPopup();
+  } catch (error) {
+    console.error("Error occurred:", error);
   }
 });
 
@@ -76,19 +75,15 @@ const showPopup = () => {
 
 const generatePDF = async (name, certificateNumber) => {
   try {
-    const existingPdfBytes = await fetch("CertificateFiks.pdf").then((res) =>
-      res.arrayBuffer()
-    );
+    const existingPdfBytes = await fetch("CertificateFiks.pdf").then((res) => res.arrayBuffer());
 
     // Load the existing PDF document
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
     pdfDoc.registerFontkit(fontkit);
 
-    const fontBytes = await fetch("AvenirNextLTPro-Regular.otf").then((res) =>
-      res.arrayBuffer()
-    );
+    const fontBytes = await fetch("AvenirNextLTPro-Regular.otf").then((res) => res.arrayBuffer());
     // Embed the font into the PDF document
-    const SanChezFont = await pdfDoc.embedFont(fontBytes);
+    const sanChezFont = await pdfDoc.embedFont(fontBytes);
 
     // Get the first page of the document
     const pages = pdfDoc.getPages();
@@ -98,12 +93,12 @@ const generatePDF = async (name, certificateNumber) => {
     const currentDate = new Date();
     const lastTwoDigitsOfYear = String(currentDate.getFullYear()).slice(-2);
     const formattedDate = `${lastTwoDigitsOfYear}/${currentDate.getMonth() + 1}/${currentDate.getDate()}`;
-    
+
     firstPage.drawText(name, {
       x: 130,
       y: 1035,
       size: 36,
-      font: SanChezFont,
+      font: sanChezFont,
       color: rgb(1, 1, 1),
     });
 
@@ -111,7 +106,7 @@ const generatePDF = async (name, certificateNumber) => {
       x: 170,
       y: 1370,
       size: 22,
-      font: SanChezFont,
+      font: sanChezFont,
       color: rgb(1, 1, 1),
     });
 
@@ -124,7 +119,6 @@ const generatePDF = async (name, certificateNumber) => {
     throw error;
   }
 };
-
 
 const sendCertificateData = async (name, email, telepon, certificateNumber, pdfDataUri) => {
   try {
@@ -140,7 +134,7 @@ const downloadCertificates = async () => {
 
     for (let i = 0; i < data.length; i++) {
       const nextCertificateNumber = await getNextCertificateNumber();
-      const pdfDataUri = await generatePDF("PlaceholderName", nextCertificateNumber);
+      const pdfDataUri = await generatePDF(data[i].name, nextCertificateNumber);
 
       zip.file(`Certificate_${nextCertificateNumber}.pdf`, pdfDataUri.split("base64,")[1], { base64: true });
     }
@@ -178,10 +172,10 @@ const getNextCertificateNumber = async () => {
         `Failed to fetch next certificate number. Server response: ${response.statusText}`
       );
     }
-    const data = await response.json();
+    const responseData = await response.json();
 
-    if (data && data.nextCertificateNumber !== undefined) {
-      return data.nextCertificateNumber;
+    if (responseData && responseData.nextCertificateNumber !== undefined) {
+      return responseData.nextCertificateNumber;
     } else {
       throw new Error("Invalid response format from the server");
     }
